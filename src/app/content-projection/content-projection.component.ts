abceaef41bd74f4736c23412a6748a6f6a24815e
aamirubit <aamirsaleem167@gmail.com>
2017-09-12 03:08:23 +0500

    final

--- src/app/content-projection/content-projection.component.ts
+++ src/app/content-projection/content-projection.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-content-projection',
@@ -6,9 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content-projection.component.css']
 })
 export class ContentProjectionComponent implements OnInit {
-
   constructor() { }
-
   ngOnInit() {
+  }
+
+  handleLogin(value: any) {
+       console.log('Login' + value);
+  }
+  handleRegister(value: any) {
+       console.log('Register' + value);
   }
 }
--- src/app/content-projection/form-content/form-content.component.ts
+++ src/app/content-projection/form-content/form-content.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {  OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, ContentChildren, ViewChild, QueryList, AfterViewInit, Input } from '@angular/core';
+import { NgModel, NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-form-content',
   templateUrl: './form-content.component.html',
   styleUrls: ['./form-content.component.css']
 })
-export class FormContentComponent implements OnInit {
+export class FormContentComponent implements OnInit, AfterViewInit {
 
+  @Output() userSubmit= new EventEmitter<any>();
+  @ContentChildren(NgModel) public models: QueryList<NgModel>;
+  @ViewChild(NgForm) public form: NgForm;
+
+  public ngAfterViewInit(): void {
+    const ngContentModels = this.models.toArray();
+    ngContentModels.forEach((model) => {
+      this.form.addControl(model);
+    });
+  }
   constructor() { }
 
   ngOnInit() {
   }
+  public onSubmit(editForm: any): void {
+    console.log(editForm);
+  }
+
+  // onSubmit(value: any) {
+  //   //  console.log(value);
+  //    this.userSubmit.emit(value);
+  // }
 
 }
